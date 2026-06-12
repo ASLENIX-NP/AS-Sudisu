5a26a2f66e70068a3e776fc219105e1d3d92b7b8
alisha-saud <alishasaud6@gmail.com>
2026-06-12 10:20:02 +0545

    bugfixed

--- Project Aslenix/backend/src/routes/authRoutes.js
+++ Project Aslenix/backend/src/routes/authRoutes.js
@@ -1,18 +1,12 @@
 import express from "express";
 import {
-register,
-login,
-forgotPassword,
-resetPassword,
+  register,
+  login,
 } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-
-/* Password Reset */
-router.post("/forgot-password", forgotPassword);
-router.post("/reset-password/:token", resetPassword);
 
 export default router;
